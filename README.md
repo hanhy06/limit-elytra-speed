a6033e548db289e12757598c0510a846cf7ac0aa
조환희 <whghksgmls@gmail.com>
2025-12-23 19:30:11 +0900

    Update Minecraft version to 1.20 and refactor Elytra speed limit implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Sets the maximum speed of the Elytra in Minecraft. A new game rule named limit_elytra_speed is added, and its unit is blocks per second.
+This mod lets you set the maximum speed of the Elytra in Minecraft.
+It adds a new game rule, **limit_elytra_speed**, measured in blocks per second.
+By configuring this value, you can prevent abnormal high-speed movement and improve overall server stability.
--- src/main/java/io/github/hanhy06/limitelytraspeed/LimitElytraSpeed.java
+++ src/main/java/io/github/hanhy06/limitelytraspeed/LimitElytraSpeed.java
@@ -1,53 +1,51 @@
 package io.github.hanhy06.limitelytraspeed;
 
 import net.fabricmc.api.ModInitializer;
+
 import net.fabricmc.fabric.api.entity.event.v1.EntityElytraEvents;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleBuilder;
-import net.fabricmc.fabric.api.gamerule.v1.GameRuleEvents;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.Identifier;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
-import net.minecraft.world.rule.GameRule;
-import net.minecraft.world.rule.GameRuleCategory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LimitElytraSpeed implements ModInitializer {
-	public static final String MOD_ID = "limit-elytra-speed";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "limit-elytra-speed";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final GameRule<Integer> LIMIT_ELYTRA_SPEED = GameRuleBuilder
-            .forInteger(70)
-            .category(GameRuleCategory.PLAYER)
-            .buildAndRegister(Identifier.of("limit_elytra_speed"));
+    public static final GameRules.Key<GameRules.IntRule> LIMIT_ELYTRA_SPEED = GameRuleRegistry.register(
+            "limit_elytra_speed",
+            GameRules.Category.PLAYER,
+            GameRuleFactory.createIntRule(70)
+    );
 
-    public static double limitSpeed = 70;
-
-	@Override
-	public void onInitialize() {
-        GameRuleEvents.changeCallback(LIMIT_ELYTRA_SPEED).register(LimitElytraSpeed::calculateSpeed);
+    @Override
+    public void onInitialize() {
         EntityElytraEvents.CUSTOM.register(LimitElytraSpeed::LimitSpeed);
 
-		LOGGER.info(MOD_ID + "Loaded");
-	}
-
-    private static void calculateSpeed(Integer integer, MinecraftServer server){
-        double limitPerTick = integer / 20.0;
-        limitSpeed = limitPerTick * limitPerTick;
+        LOGGER.info(MOD_ID + "Loaded");
     }
 
     public static boolean LimitSpeed(LivingEntity entity, boolean tickElytra){
         World world = entity.getEntityWorld();
         if (world.isClient()) return tickElytra;
 
+        ServerWorld serverWorld = world.getServer().getWorld(world.getRegistryKey());
+        int limitSpeed = serverWorld.getGameRules().getInt(LIMIT_ELYTRA_SPEED);
+
+        double limitPerTick = limitSpeed / 20.0;
+        double limitPerTickSquared = limitPerTick * limitPerTick;
+
         Vec3d currentVelocity = entity.getVelocity();
         double currentVelocitySquared = currentVelocity.lengthSquared();
 
-        if (currentVelocitySquared > limitSpeed) {
-            entity.setVelocity(currentVelocity.normalize().multiply(limitSpeed));
-            entity.velocityDirty = true;
+        if (currentVelocitySquared > limitPerTickSquared) {
+            entity.setVelocity(currentVelocity.normalize().multiply(limitPerTick));
+            entity.velocityModified = true;
         }
 
         return tickElytra;
